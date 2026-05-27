d9008b848fb970dbc4a4fd55f3c800f74c07123c
Mark Davis <Mark@DesignerMarkDavis.com>
2026-05-26 23:39:46 -0400

    rename header to ReCal Sans

--- src/App.tsx
+++ src/App.tsx
@@ -309,7 +309,7 @@ export default function App() {
   return (
     <div className="app">
       <header>
-        <h1>ReCal</h1>
+        <h1>ReCal Sans</h1>
         <p className="subtitle">Cal Sans Customizer</p>
       </header>
 
